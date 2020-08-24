fd29766c5861f8084219612914640df68325c3a1
laczkobalazs <laczko.balazs89@gmail.com>
2020-08-24 16:03:17 +0200

    modifies authcontroller crossorigin

--- src/main/java/com/codecool/moviedb/controller/AuthController.java
+++ src/main/java/com/codecool/moviedb/controller/AuthController.java
@@ -18,7 +18,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin("*")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
