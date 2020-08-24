480bf58923bcd001e6f91a22c64f850b2d437900
Steven Trautmann <steven.trautmann1@gmail.com>
2020-08-24 14:55:18 +0200

    sign in route added

---
+++ src/main/java/com/codecool/moviedb/controller/AuthController.java
@@ -0,0 +1,57 @@
+package com.codecool.moviedb.controller;
+
+import com.codecool.moviedb.repository.UserRepository;
+import com.codecool.moviedb.security.JwtTokenServices;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices, UserRepository users) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity signIn(@RequestBody Map<String, Object> data) {
+        System.out.println("request body: " + data);
+        try {
+            String username = data.get("username").toString();
+            String password = data.get("password").toString();
+            // authenticationManager.authenticate calls loadUserByUsername in CustomUserDetailsService
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            List<String> roles = authentication.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+
+            String token = jwtTokenServices.createToken(username, roles);
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("roles", roles);
+            model.put("token", token);
+            return ResponseEntity.ok(model);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+}
