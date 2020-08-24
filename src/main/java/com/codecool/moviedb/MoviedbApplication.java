e9a4d11c3872fcc26e6d3d68ff1a5244c994a537
Steven Trautmann <steven.trautmann1@gmail.com>
2020-08-24 14:53:25 +0200

    upgraded dummy user

--- src/main/java/com/codecool/moviedb/MoviedbApplication.java
+++ src/main/java/com/codecool/moviedb/MoviedbApplication.java
@@ -8,12 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class MoviedbApplication {
 
     @Autowired
     UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     public static void main(String[] args) {
         SpringApplication.run(MoviedbApplication.class, args);
@@ -23,7 +29,9 @@ public class MoviedbApplication {
     @Profile("production")
     public CommandLineRunner init(){
         return args -> {
-            User user = User.builder().userName("Isti").password("ugyi").build();
+            String encodePassword = passwordEncoder.encode("ugyi");
+            User user = User.builder().userName("Isti").password(encodePassword).build();
+            user.setRoles(Arrays.asList("ROLE_USER"));
             userRepository.save(user);
         };
 
