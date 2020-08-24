1cd3cb48db6bd642c80814714d1ecee78bfe6d29
Steven Trautmann <steven.trautmann1@gmail.com>
2020-08-24 14:55:00 +0200

    added new query

--- src/main/java/com/codecool/moviedb/repository/UserRepository.java
+++ src/main/java/com/codecool/moviedb/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.codecool.moviedb.repository;
 import com.codecool.moviedb.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserName(String username);
 }
