8090efd40bb0deadb4283083f4f3f57f352a5e73
Viktor Bocz <beviktor95@gmail.com>
2020-08-25 14:50:44 +0200

    Failed trying to set cookie on backend

--- src/main/java/com/codecool/moviedb/controller/AuthController.java
+++ src/main/java/com/codecool/moviedb/controller/AuthController.java
@@ -15,6 +15,8 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -41,7 +43,7 @@ public class AuthController {
     }
 
     @PostMapping("/sign-in")
-    public ResponseEntity signIn(@RequestBody Map<String, Object> data) {
+    public ResponseEntity signIn(@RequestBody Map<String, Object> data, HttpServletResponse response) {
         try {
             String username = data.get("username").toString();
             String password = data.get("password").toString();
@@ -58,6 +60,15 @@ public class AuthController {
             model.put("username", username);
             model.put("roles", roles);
             model.put("token", token);
+
+            Cookie cookie = new Cookie("Authorization", token);
+            cookie.setPath("/");
+            cookie.setMaxAge(24*60*60);
+            cookie.setSecure(true);
+            cookie.setHttpOnly(true);
+            System.out.println(cookie.getValue());
+            response.addCookie(cookie);
+
             return ResponseEntity.ok(model);
         } catch (AuthenticationException e) {
             throw new BadCredentialsException("Invalid username/password supplied");
@@ -65,7 +76,7 @@ public class AuthController {
     }
 
     @PostMapping("/registration")
-    public ResponseEntity register(@RequestBody Map<String, Object> data) {
+    public ResponseEntity register(@RequestBody Map<String, Object> data, HttpServletResponse response) {
         String username = data.get("username").toString();
         String password = data.get("password").toString();
         String email = data.get("email").toString();
@@ -75,9 +86,19 @@ public class AuthController {
             String token = jwtTokenServices.createToken(username, Arrays.asList("ROLE_USER"));
 
             Map<Object, Object> model = new HashMap<>();
+
             model.put("username", username);
             model.put("roles", Arrays.asList("ROLE_USER"));
             model.put("token", token);
+
+            Cookie cookie = new Cookie("Authorization", token);
+            cookie.setPath("/");
+            cookie.setMaxAge(24*60*60);
+            cookie.setSecure(true);
+            cookie.setHttpOnly(true);
+            System.out.println(cookie.getValue());
+            response.addCookie(cookie);
+
             return ResponseEntity.ok(model);
         } catch (Exception e) {
             throw new BadCredentialsException("Username/email address already in use!");
